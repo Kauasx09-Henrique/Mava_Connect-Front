80ef1f7899fc7abe8b92ec230bf681d02feceaf8
Kauã Henrique <kauahenriquesx09@gmail.com>
2025-07-31 08:30:14 -0300

    teste

--- Pages/Secretaria.jsx
+++ Pages/Secretaria.jsx
@@ -9,7 +9,7 @@ import Header from '../src/Components/Header';
 import styles from './style/Secretaria.module.css';
 import { FiEdit, FiTrash2, FiPhone, FiMail, FiPlus, FiX, FiCalendar } from 'react-icons/fi';
 import { useIMask } from 'react-imask';
-import { useViaCep } from '../hooks/useViaCep';
+import { useViaCep } from '../src/hooks/useViaCep';
 
 const API_URL = 'https://mava-connect-backend.onrender.com';
 
